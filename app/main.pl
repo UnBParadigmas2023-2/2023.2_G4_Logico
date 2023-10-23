b0293548b03014e6bf81f0a05f3e13781ecbb312
JoaoPedro0803 <joaopedrocvaz@gmail.com>
2023-10-23 14:28:14 -0300

    Adicionando predicado opcaoInvalida para tratar entrada errada

--- app/main.pl
+++ app/main.pl
@@ -5,8 +5,14 @@ menu_principal :-
     nl,read(OpcaoMenu),
     (   OpcaoMenu =:= 1
     ->  write("Você selecionou a opção 1")
-    ;   sair
+    ;   OpcaoMenu =:= 2
+    ->  sair
+    ;   opcaoInvalida
     ).
 
 sair :- 
     nl,write("Obrigado por usar nosso recomendador!").
+
+opcaoInvalida :-
+    nl,write("Voce não digitou uma opção válida!"),
+    nl,menu_principal.
