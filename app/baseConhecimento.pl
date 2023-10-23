5493325782fc127a41b4a2702e09af95cb02a885
GabrielRoger07 <gabrielroger4203@gmail.com>
2023-10-23 14:58:06 -0300

    Adicionando arquivo de base de conhecimento

---
+++ app/baseConhecimento.pl
@@ -0,0 +1,5 @@
+filme('Gato de Botas 2', 'animacao', 2022, 73, 'Joel Crawford').     % O filme Gato de Botas 2 é uma animação lançada em 2022 que possui avaliação 73 no Metacritic e tem  como seu diretor 
+filme('Pantera Negra', 'acao', 2018, 88, 'Ryan Coogler').
+filme('Vingadores Ultimato', 'acao', 2019, 78, 'Joe Russo').
+filme('Se Beber Nao Case', 'comedia', 2009, 30, 'Todd Phillips').
+filme('Barbie', 'comedia', 2023, 81, 'Greta Gerwig').
