a0e703849d8b4fbc8d256355011112fa8d730e15
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-10-23 15:35:14 -0300

    Adicao dos estudios na base

--- app/baseConhecimento.pl
+++ app/baseConhecimento.pl
@@ -1,5 +1,5 @@
-filme('Gato de Botas 2', 'animacao', 2022, 73, 'Joel Crawford').     % O filme Gato de Botas 2 é uma animação lançada em 2022 que possui avaliação 73 no Metacritic e tem  como seu diretor 
-filme('Pantera Negra', 'acao', 2018, 88, 'Ryan Coogler').
-filme('Vingadores Ultimato', 'acao', 2019, 78, 'Joe Russo').
-filme('Se Beber Nao Case', 'comedia', 2009, 30, 'Todd Phillips').
-filme('Barbie', 'comedia', 2023, 81, 'Greta Gerwig').
+filme('Gato de Botas 2', 'animacao', 2022, 73, 'Joel Crawford', 'Universal').     % O filme Gato de Botas 2 é uma animação lançada em 2022 que possui avaliação 73 no Metacritic, tem Joel Crawford como seu diretor e Universal como estúdio
+filme('Pantera Negra', 'acao', 2018, 88, 'Ryan Coogler', 'Marvel').
+filme('Vingadores Ultimato', 'acao', 2019, 78, 'Joe Russo', 'Marvel').
+filme('Se Beber Nao Case', 'comedia', 2009, 30, 'Todd Phillips', 'Warner Bros').
+filme('Barbie', 'comedia', 2023, 81, 'Greta Gerwig', 'Warner Bros').
