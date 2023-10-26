83b8cf8e1d2af69242960c106a577d853dda331f
Juliana Valle <juju.pvg@gmail.com>
2023-10-26 07:33:20 -0700

    feat: atualizando base de conhecimento

--- app/baseConhecimento.pl
+++ app/baseConhecimento.pl
@@ -1,5 +1,85 @@
-filme('Gato de Botas 2', 'animacao', 2022, 73, 'Joel Crawford', 'Universal').     % O filme Gato de Botas 2 é uma animação lançada em 2022 que possui avaliação 73 no Metacritic, tem Joel Crawford como seu diretor e Universal como estúdio
-filme('Pantera Negra', 'acao', 2018, 88, 'Ryan Coogler', 'Marvel').
-filme('Vingadores Ultimato', 'acao', 2019, 78, 'Joe Russo', 'Marvel').
-filme('Se Beber Nao Case', 'comedia', 2009, 30, 'Todd Phillips', 'Warner Bros').
-filme('Barbie', 'comedia', 2023, 81, 'Greta Gerwig', 'Warner Bros').
+filme('Us', 'terror', 2019, 81, 'Jordan Peele', 'Universal').     % O filme Us é um terror lançado em 2023 que possui avaliação 81 no Metacritic, tem Jordan Peele como seu diretor e Universal como estúdio
+filme('Umma', 'drama', 2022, 51, 'Iris K. Shim', 'Sony').
+filme('Zombieland', 'comedia', 2009, 73, 'Ruben Fleischer', 'Columbia').
+filme('Halloween', 'terror', 1978, 87, 'John Carpenter', 'Aquarius Releasing').
+filme('A Casa Monstro', 'animacao', 2006, 68, 'Gil Kenan', 'Columbia').
+filme('Candyman', 'terror', 2021, 72, 'Nia DaCosta', 'Universal').
+filme('Telefone Preto', 'mistério', 2022, 65, 'Scott Derrickson', 'Universal').
+filme('O Estranho Mundo de Jack', 'animacao', 1993, 82, 'Henry Selick', 'Buena Vista').
+filme('Deu a Louca nos Monstros', 'comedia', 1987, 61, 'Fred Dekker', 'TriStar').
+filme('A Noiva-Cadáver', 'animacao', 2005, 83, 'Tim Burton', 'Warner Bros.').
+filme('Coraline', 'animacao', 2009, 80, 'Henry Selick', 'Focus Features').
+filme('O Jovem Frankenstein', 'comedia', 1974, 83, 'Mel Brooks', 'Twentieth Century Fox').
+filme('O Bebê de Rosemary', 'terror', 1968 , 96, 'Roman Polanski', 'Paramount').
+filme('A Família Addams', 'comedia', 1991 , 57, 'Barry Sonnenfeld', 'Paramount').
+filme('Entrevista com o Vampiro', 'drama', 1994 , 59, 'Neil Jordan', 'Geffen').
+filme('O Massacre da Serra Elétrica', 'terror', 1974 , 89, 'Tobe Hooper', 'Bryanston Distributing').
+filme('Crepúsculo', 'romance', 2008 , 56, 'Catherine Hardwicke', 'Summit Distribution').
+filme('Frankenstein', 'sci-fi', 1931 , 91, 'James Whale', 'Universal').
+filme('Invocação do Mal', 'terror', 2013 , 68, 'James Wan', 'New Line Cinema').
+filme('The Rocky Horror Picture Show', 'musical', 1975 , 65, 'Jim Sharman', 'Twentieth Century Fox').
+filme('O Mágico de Oz', 'musical', 1939 , 92, 'Victor Fleming', 'Metro-Goldwyn-Mayer (MGM)').
+filme('King Kong', 'sci-fi', 1933 , 92, 'Merian C. Cooper', 'RKO Radio Pictures').
+filme('Os Olhos Sem Rosto', 'drama', 1960 , 90, 'Georges Franju', 'Lopert Pictures Corporation').
+filme('Poltergeist', 'terror', 1982 , 79, 'Tobe Hooper', 'MGM/UA Entertainment Company').
+filme('Convenção das Bruxas', 'fantasia', 1990 , 78, 'Nicolas Roeg', 'Warner Bros.').
+filme('Dracula', 'fantasia', 1931 , 71, 'Tod Browning', 'Universal').
+filme('Desafio do Além', 'terror', 1963 , 74, 'Robert Wise', 'Metro-Goldwyn-Mayer (MGM)').
+filme('Da Magia à Sedução', 'fantasia', 1998 , 46, 'Griffin Dunne', 'Warner Bros.').
+filme('Corra!', 'mistério', 2017 , 85, 'Jordan Peele', 'Universal').
+filme('Noites Brutais', 'mistério', 2022 , 78, 'Zach Cregger', '20th Century Studios').
+filme('Brinquedo Assassino', 'terror', 1988 , 58, 'Tom Holland', 'MGM/UA Communications Co.').
+filme('Sexta-Feira 13', 'terror', 1980 , 22, 'Sean S. Cunningham', ' Paramount').
+filme('Psicose', 'mistério', 1960 , 97, 'Alfred Hitchcock', ' Paramount').
+filme('Atividade Paranormal', 'terror', 2009 , 97, 'Oren Peli', ' Paramount').
+filme('A Lenda do Cavaleiro sem Cabeça', 'fantasia', 1999 , 65, 'Tim Burton', ' Paramount').
+filme('Um Lugar Silencioso', 'suspense', 2018 , 82, 'John Krasinski', ' Paramount').
+filme('A Hora do Pesadelo', 'terror', 1984 , 82, 'Wes Craven', 'New Line Cinema').
+filme('O Exorcista', 'terror', 1973 , 82, 'Wes Craven', 'Warner Bros.').
+filme('It - A Coisa', 'terror', 2017 , 82, 'Andy Muschietti', 'New Line Cinema').
+filme('Carrie - A Estranha', 'terror', 1976 , 86, 'Brian De Palma', 'United Artists').
+filme('Edward Mãos de Tesoura', 'drama', 1990, 74, 'Tim Burton', 'Twentieth Century Fox').
+filme('Os Caça-Fantasmas', 'comédia', 1984, 71, 'Ivan Reitman', 'Columbia').
+filme('Pânico', 'terror', 1996, 65, 'Wes Craven', 'AMC Theatres').
+filme('Abracadabra', 'comédia', 1993, 43, 'Kenny Ortega', 'Buena Vista').
+filme('Beetlejuice', 'comédia', 1988, 43, 'Tim Burton', 'Warner Bros.').
+filme('O Iluminado', 'suspense', 1980, 66, 'Stanley Kubrick', 'Warner Bros.').
+filme('Blade', 'ação', 1998, 47, 'Stephen Norrington', 'New Line Cinema').
+filme('M3GAN', 'sci-fi', 2023, 72, 'Gerard Johnstone', 'Universal').
+filme('A Bruxa de Blair', 'terror', 1999, 81, 'Daniel Myrick', 'Artisan Entertainment').
+filme('O Sexto Sentido', 'drama', 1999, 64, 'M. Night Shyamalan', 'Buena Vista').
+filme('X - A Marca da Morte', 'terror', 2022, 79, 'Ti West', 'A24').
+filme('Pearl', 'terror', 2022, 76, 'Ti West', 'A24').
+filme('Hereditário', 'terror', 2018, 87, 'Ari Aster', 'A24').
+filme('Sorria', 'terror', 2022, 68, 'Parker Finn', 'Paramount').
+filme('Morte Morte Morte', 'comédia', 2022, 69, 'Halina Reijn', 'A24').
+filme('Midsommar', 'suspense', 2019, 72, 'Ari Aster', 'A24').
+filme('Men: Faces do Medo', 'suspense', 2022, 65, 'Alex Garland', 'A24').
+filme('O farol', 'drama', 2019, 83, 'Robert Eggers', 'A24').
+filme('Fale comigo', 'terror', 2023, 76, 'Danny Philippou', 'A24').
+filme('A Bruxa', 'suspense', 2016, 84, 'Robert Eggers', 'A24').
+filme('Não! Não Olhe!', 'sci-fi', 2022, 77, 'Jordan Peele', 'Universal').
+filme('The Wailing', 'suspense', 2016, 81, 'Na Hong-jin', 'Tubi TV').
+filme('Invasão Zumbi', 'ação', 2016, 73, 'Sang-ho Yeon', 'Well Go USA Entertainment').
+filme('A Tale of Two Sisters', 'drama', 2004, 65, 'Jee-woon Kim', 'Tartan Films').
+filme('Gonjiam', 'terror', 2018, 69, 'Jung Bum-shik', 'Well Go USA Entertainment').
+filme('A Ligação', 'suspense', 2020, 71, 'Chung-Hyun Lee', 'Netflix').
+filme('Parasita', 'drama', 2019, 96, 'Bong Joon Ho', 'Neon').
+filme('A Médium', 'terror', 2021, 64, 'Banjong Pisanthanakun', 'Shudder').
+filme('Os outros', 'mistério', 2001, 74, 'Alejandro Amenábar', 'Miramax').
+filme('Speak No Evil', 'suspense', 2022, 78, 'Christian Tafdrup', 'Shudder').
+filme('Cisne Negro', 'drama', 2010, 79, 'Darren Aronofsky', 'Searchlight').
+filme('Soft & Quiet', 'drama', 2022, 82, 'Beth de Araújo', 'Momentum').
+filme('Bacurau', 'suspense', 2020, 82, 'Juliano Dornelles', 'Kino Lorber').
+filme('Requiem', 'terror', 2006, 82, 'Hans-Christian Schmid', 'IFC First Take').
+filme('Mad God', 'animação', 2022, 80, 'Phil Tippett', 'IFC Midnight').
+filme('Creepy', 'mistério', 2016, 76, 'Kiyoshi Kurosawa', 'Kimstim Films').
+filme('mother!', 'drama', 2017, 76, 'Darren Aronofsky', 'Paramount').
+filme('O Poço', 'mistério', 2020, 73, 'Galder Gaztelu-Urrutia', 'Netflix').
+filme('O Menu', 'comédia', 2022, 71, 'Mark Mylod', 'Walt Disney Studios').
+filme('Dezesseis Facadas', 'comédia', 2023, 62, 'Nahnatchka Khan', 'Amazon Prime Video').
+filme('Fresh', 'comédia', 2022, 67, 'Mimi Cave', 'Hulu').
+filme('Scooby-Doo', 'aventura', 2002, 35, 'Raja Gosnell', 'Warner Bros.').
+filme('Goosebumps: Monstros e Arrepios', 'aventura', 2015, 60, 'Rob Letterman', 'Columbia').
+filme('#Alive', 'ação', 2020, 63, 'Il Cho', 'Netflix').
+filme('Constantine', 'ação', 2005, 50, 'Francis Lawrence', 'Warner Bros.').
