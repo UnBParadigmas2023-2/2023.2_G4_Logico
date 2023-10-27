510dac4544747ca5e3c90b53a50125a7e8e1bf75
Julio Cesar <julio0033@hotmail.com>
2023-10-27 12:36:57 -0300

    Mudança na forma de remover repetições das listas

--- app/regrasAuxiliares.pl
+++ app/regrasAuxiliares.pl
@@ -3,18 +3,17 @@
 % Regra para obter uma lista de todos os gêneros de filmes na base de dados sem repetições
 todos_generos(Generos) :-
     findall(Genre, filme(_, Genre, _, _, _, _), ListaGeneros),
-    remove_repeticoes(ListaGeneros, Generos).
+    sort(ListaGeneros, Generos).
 
 % Regra para obter uma lista de todos os diretores de filmes na base de dados sem repetições
 todos_diretores(Diretores) :-
     findall(Diretor, filme(_, _, _, _, Diretor, _), ListaDiretores),
-    remove_repeticoes(ListaDiretores, Diretores).
+    sort(ListaDiretores, Diretores).
 
 % Regra para obter uma lista de todos os estudios de filmes na base de dados sem repetições
 todos_estudios(Estudios) :-
     findall(Estudio, filme(_, _, _, _, _, Estudio), ListaEstudios),
-    remove_repeticoes(ListaEstudios, Estudios).
-
+    sort(ListaEstudios, Estudios).
 % Regra para exibir gêneros
 mostrar_generos :-
     todos_generos(Generos),
@@ -29,12 +28,6 @@ mostrar_diretores :-
 mostrar_estudios :-
     todos_estudios(Estudios),
     formatar_atributos(Estudios).
-
-% Regra para remover repetições de uma lista
-remove_repeticoes([], []).
-remove_repeticoes([H | T], Resultado) :-
-    (member(H, T) -> remove_repeticoes(T, Resultado) ; Resultado = [H | Resto]),
-    remove_repeticoes(T, Resto).
 
 % Regra formatar atributos
 formatar_atributos([]).
